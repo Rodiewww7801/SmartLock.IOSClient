4fb4ea0d184fae3275da6000b7fb7595505aed44
Rodion Hladchenko <r.hladchenko@pnn.in.ua>
2023-03-28 23:38:16 +0300

    finish UI on MainCameraVC

--- FaceDetectionIOS/User Interface/MainCameraScene/MainCameraVC.swift
+++ FaceDetectionIOS/User Interface/MainCameraScene/MainCameraVC.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class MainCameraVC: UIViewController {
     private var lastPhotoButton: UIButton = UIButton()
-    private var capturePhotoView: FDFadeAnimatedButton!
+    private var capturePhotoView: FDFadeAnimatedButton = FDFadeAnimatedButton()
     private var changeCameraButton: UIButton = UIButton()
     private var bottomButtonsFirstStack: UIStackView = UIStackView()
-    private var hideBackgroundButton: UIButton = UIButton()
-    private var debugModeButton: UIButton = UIButton()
+    private var hideBackgroundButton: FDFadeAnimatedButton = FDFadeAnimatedButton()
+    private var hideBackgroundLabel: UILabel = UILabel()
+    private var debugModeButton: FDFadeAnimatedButton = FDFadeAnimatedButton()
+    private var debugModeLabel: UILabel = UILabel()
     private var bottomButtonsSecondStack: UIStackView = UIStackView()
+    private var faceDetectionStateLabel: UILabel = UILabel()
     
     override func viewDidLoad() {
         self.view.backgroundColor = .black
@@ -23,6 +26,8 @@ class MainCameraVC: UIViewController {
     
     private func configureViews() {
         configureBottomButtonsFirstStack()
+        configureBottomButtonsSecondStack()
+        configureFaceDetectionStateLabel()
     }
     
     private func configureBottomButtonsFirstStack() {
@@ -92,6 +97,48 @@ class MainCameraVC: UIViewController {
     }
     
     private func configureBottomButtonsSecondStack() {
+        self.bottomButtonsSecondStack = UIStackView()
+        self.bottomButtonsSecondStack.alignment = .center
+        self.bottomButtonsSecondStack.distribution = .equalCentering
+        self.bottomButtonsSecondStack.spacing = 25
+        self.bottomButtonsSecondStack.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(bottomButtonsSecondStack)
+        self.bottomButtonsSecondStack.bottomAnchor.constraint(equalTo: self.bottomButtonsFirstStack.topAnchor, constant: -15).isActive = true
+        self.bottomButtonsSecondStack.centerXAnchor.constraint(equalTo: self.bottomButtonsFirstStack.centerXAnchor).isActive = true
         
+        // add hideBackgroundLabel
+        self.hideBackgroundLabel = UILabel()
+        self.hideBackgroundLabel.text = "hide background".uppercased()
+        self.hideBackgroundLabel.textColor = .white
+        self.hideBackgroundLabel.font = .systemFont(ofSize: 14)
+        self.hideBackgroundLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.hideBackgroundButton = FDFadeAnimatedButton()
+        self.hideBackgroundButton.addSubview(hideBackgroundLabel)
+        self.bottomButtonsSecondStack.addArrangedSubview(hideBackgroundButton)
+        self.hideBackgroundButton.widthAnchor.constraint(equalTo: hideBackgroundLabel.widthAnchor).isActive = true
+        self.hideBackgroundButton.heightAnchor.constraint(equalTo:  hideBackgroundLabel.heightAnchor).isActive = true
+        
+        // add hideBackgroundLabel
+        self.debugModeLabel = UILabel()
+        self.debugModeLabel.text = "debug mode".uppercased()
+        self.debugModeLabel.textColor = .white
+        self.debugModeLabel.font = .systemFont(ofSize: 14)
+        self.debugModeLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.debugModeButton = FDFadeAnimatedButton()
+        self.debugModeButton.addSubview(debugModeLabel)
+        self.bottomButtonsSecondStack.addArrangedSubview(debugModeButton)
+        self.debugModeButton.widthAnchor.constraint(equalTo: debugModeLabel.widthAnchor).isActive = true
+        self.debugModeButton.heightAnchor.constraint(equalTo: debugModeLabel.heightAnchor).isActive = true
+    }
+    
+    private func configureFaceDetectionStateLabel() {
+        self.faceDetectionStateLabel = UILabel()
+        self.faceDetectionStateLabel.text = "Please go fuck yourself"
+        self.faceDetectionStateLabel.textColor = .white
+        self.faceDetectionStateLabel.font = .systemFont(ofSize: 18, weight: .light)
+        self.faceDetectionStateLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(faceDetectionStateLabel)
+        self.faceDetectionStateLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.faceDetectionStateLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
     }
 }
